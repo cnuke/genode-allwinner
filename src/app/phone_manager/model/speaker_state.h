38c4def475d3f39e25b5fb52c4282b711cbb0202
Josef Söntgen <josef.soentgen@genode-labs.com>
2023-11-02 13:48:50 +0100

    phone_manager: add speaker configuration menu
    The speaker options configure if the big speaker on the back of the
    PinePhone is used in addition to the small speaker on the front at the
    top.
    
    The default setting, 'Ring', configures its usage in a way that is
    useful for regular phone-calls where the big speaker is enabled for
    playing the ring-tones but is muted during a call. 'On' and 'Off'
    control the speaker explicitly. To prevent unexpected echo issues where
    the microphone at the back of the PinePhone picks received audio the
    'Off' setting should be used.

---
+++ src/app/phone_manager/model/speaker_state.h
@@ -0,0 +1,21 @@
+/*
+ * \brief  Speaker state
+ * \author Josef Soentgen
+ * \date   2023-11-02
+ */
+
+/*
+ * Copyright (C) 2023 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _MODEL__SPEAKER_STATE_H_
+#define _MODEL__SPEAKER_STATE_H_
+
+#include <types.h>
+
+namespace Sculpt { enum class Speaker_state { OFF, RING, ON }; }
+
+#endif /* _MODEL__SPEAKER_STATE_H_ */
